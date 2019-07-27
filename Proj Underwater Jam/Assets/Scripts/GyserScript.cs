30b19596463504a6e0db2e641a8687c9002a60c9
FTWRahul <r.poundarik@gmail.com>
2019-07-26 23:26:34 -0400

    Initial commit

---
+++ Proj Underwater Jam/Assets/Scripts/GyserScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyserScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxForce;
+    [SerializeField]
+    float forceScale;
+    [SerializeField]
+    float forceAdder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (forceAdder < maxForce)
+        {
+            forceAdder += forceScale * Time.deltaTime;
+        }
+        other.GetComponent<ConstantForce>().force = new Vector3(0, forceAdder, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<ConstantForce>().force = Vector3.zero;
+    }
+}
---
+++ Proj Underwater Jam/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(ConstantForce))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+    [SerializeField]
+    float rotationSpeed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float input = Input.GetAxis("Horizontal");
+        rb.AddTorque(new Vector3(0, 0, -input * rotationSpeed * 100 * Time.deltaTime));
+        rb.AddForce(new Vector3(input * movementSpeed * 100 * Time.deltaTime, 0, 0));
+    }
+}
