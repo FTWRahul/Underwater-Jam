9f2b0aec4acaddcb960669a556bc66dfb6403eb2
Amanda <amandaduarte2012@gmail.com>
2019-07-27 17:26:01 -0400

    Voice Control Set up and Settings Pannel Sliders finish
    - need to add the sounds and assign the mixers only -

---
+++ Proj Underwater Jam/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetMasterLv(float masterLvl)
+    {
+        masterMixer.SetFloat("masterVol", masterLvl);
+    }
+
+    public void SetSfxLv(float sfxLvl)
+    {
+        masterMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMusicLv(float musicLvl)
+    {
+        masterMixer.SetFloat("musicVol", musicLvl);
+    }
+
+    public void SetNarratorLv(float narratorLvl)
+    {
+        masterMixer.SetFloat("narratorVol", narratorLvl);
+    }
+}
